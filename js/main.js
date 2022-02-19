c49f094a1cbfc4000afdb66eec1f6141534e2a05
apuggioni <agustinpuggioni98@gmail.com>
2022-02-19 19:30:18 -0300

    map and mixin

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 //RESPONSIVE NAV
-const navSlide = () =>{
-    const burger = document.querySelector ('.burger');
-    const nav = document.querySelector('.nav-main');
-    const navLinks = document.querySelectorAll('.nav-main li')
-   
-    burger.addEventListener('click',()=>{
-        //Toggle
-        nav.classList.toggle('nav-active');
-        //Burger animation
-        burger.classList.toggle('toggle');
-    });
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-main");
+  const navLinks = document.querySelectorAll(".nav-main li");
 
-}
+  burger.addEventListener("click", () => {
+    //Toggle
+    nav.classList.toggle("nav-active");
+    //Burger animation
+    burger.classList.toggle("toggle");
+  });
+  //Animated Links
+  navLinks.forEach((link, index) => {
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 2}s`;
+  });
+};
 
 navSlide();
