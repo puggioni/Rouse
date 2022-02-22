3503fef9eb2d1b0809d59316cc75eaf10b5ceb72
apuggioni <agustinpuggioni98@gmail.com>
2022-02-21 22:46:59 -0300

    display none al nav-main y arregle el burger

--- js/main.js
+++ js/main.js
@@ -10,10 +10,6 @@ const navSlide = () => {
     //Burger animation
     burger.classList.toggle("toggle");
   });
-  //Animated Links
-  navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 2}s`;
-  });
 };
 
 navSlide();
